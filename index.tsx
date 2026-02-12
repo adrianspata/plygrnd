e74cc768301c840ba044f0c2e36387f96e73c6b9
adrianspata <adrian.spata@hotmail.com>
2026-02-12 11:38:33 +0100

    feat: Add a console signature and use an environment variable for the Web3Forms access key.

--- index.tsx
+++ index.tsx
@@ -3,6 +3,10 @@ import { App } from "./App";
 import {
   HelmetProvider,
 } from "react-helmet-async"
+import { showConsoleSignature } from "./src/lib/consoleSignature";
 
 const container = document.getElementById("root") as HTMLDivElement;
 createRoot(container).render(<HelmetProvider><App /></HelmetProvider>);
+
+// Show console signature Easter egg
+showConsoleSignature();
